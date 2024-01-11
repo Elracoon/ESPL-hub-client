61b9aa4316a7ba5df3e98458474c0badd5133aa4
arthur <arthur.broudoux@gmail.com>
2024-01-11 13:49:21 +0100

    add theme

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,11 +1,7 @@
-import Card from "@/components/Card";
-
-
 export default function Dashboard() {
 	return (
 		<section>
 			<h1 className="text-3xl lg:text-2xl">Dashboard</h1>
-			<Card title="Card Title"/>
 		</section>
-  	);
-};
+  	)
+}
