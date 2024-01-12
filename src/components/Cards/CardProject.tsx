5e4f1620a53cd7477765f7e9c6b1d47c3bb4729d
AubinManceau <aubinmanceau0@gmail.com>
2024-01-12 11:46:00 +0100

    create CardProject

--- src/components/Cards/CardProject.tsx
+++ src/components/Cards/CardProject.tsx
@@ -1,13 +1,35 @@
 import { FC } from "react";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
 
 
 type CardProjectProps = {
-    title: string;
+    title?: string;
+    date?: string;
+    description?: string;
+    skills?: string;
+    id?: string;
+}
+
+const maxLenghtDescription = 230;
+const lenghtDescription = ( props:any ) => {
+    if ( props.description.length > maxLenghtDescription ) {
+        return props.description.slice(0, maxLenghtDescription) + ' ...';
+    }
+    return props.description;
 }
 
 const Card : FC<CardProjectProps> = ( props ) => {
     return (
-        <h1>{ props.title }</h1>
+        <div className="w-[31%] bg-white rounded-sm p-2">
+            <h1 className="text-white bg-black rounded-sm p-2 text-3xl">{ props.title }</h1>
+            <div className="flex justify-between my-2 items-center">
+                <p className="text-black text-xs">Créé le : { props.date }</p>
+                <p className="text-white bg-black rounded-sm p-1 text-xs">{ props.skills }</p>
+            </div>
+            <p className="text-black mb-3 text-sm">{ lenghtDescription(props) }</p>
+            <Link to={`/project/${props.id}`}><Button>En savoir plus</Button></Link>
+        </div>
     )
 }
 
