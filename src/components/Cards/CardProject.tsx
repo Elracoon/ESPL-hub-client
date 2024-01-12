7ca50e9c23b4b29c724e96173e886e96953931ba
arthur <arthur.broudoux@gmail.com>
2024-01-12 11:58:22 +0100

    Refractor Card component

--- src/components/Cards/CardProject.tsx
+++ src/components/Cards/CardProject.tsx
@@ -1,25 +1,27 @@
 import { FC } from "react";
-import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
+
+import { Button } from "@/components/ui/button";
 
 
 type CardProjectProps = {
-    title?: string;
-    date?: string;
-    description?: string;
-    skills?: string;
-    id?: string;
+    title: string;
+    date: string;
+    description: string;
+    skills: string;
+    id: string;
 }
 
-const maxLenghtDescription = 230;
-const lenghtDescription = ( props:any ) => {
-    if ( props.description.length > maxLenghtDescription ) {
-        return props.description.slice(0, maxLenghtDescription) + ' ...';
-    }
-    return props.description;
-}
+const CardProject : FC<CardProjectProps> = ( props ) => {
 
-const Card : FC<CardProjectProps> = ( props ) => {
+    const maxLenghtDescription = 230;
+    const lenghtDescription = ( props:any ) => {
+        if ( props.description.length > maxLenghtDescription ) {
+            return props.description.slice(0, maxLenghtDescription) + ' ...';
+        };
+        return props.description;
+    };
+
     return (
         <div className="w-[31%] bg-white rounded-sm p-2">
             <h1 className="text-white bg-black rounded-sm p-2 text-3xl">{ props.title }</h1>
@@ -28,10 +30,12 @@ const Card : FC<CardProjectProps> = ( props ) => {
                 <p className="text-white bg-black rounded-sm p-1 text-xs">{ props.skills }</p>
             </div>
             <p className="text-black mb-3 text-sm">{ lenghtDescription(props) }</p>
-            <Link to={`/project/${props.id}`}><Button>En savoir plus</Button></Link>
+            <Link to={`/project/${props.id}`}>
+                <Button>En savoir plus</Button>
+            </Link>
         </div>
     )
 }
 
 
-export default Card;
+export default CardProject;
