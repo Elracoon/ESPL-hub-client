39a3c8c8c7d9c9d101dbe3bc31fad22decf85952
AubinManceau <aubinmanceau0@gmail.com>
2024-01-12 16:48:54 +0100

    design notif

--- src/pages/Notifications.tsx
+++ src/pages/Notifications.tsx
@@ -6,27 +6,34 @@ export default function Notifications() {
     return (
         <section className="w-screen h-full p-4">
             <div className="tabs-content-dashboard">
+                <h1 className="uppercase text-white font-bold text-center text-4xl mb-8">Mes notifications</h1>
                 <Table>
                     <TableHeader className="w-full">
                         <TableRow className="text-stone-500 font-bold">
-                            <TableCell className="w-1/4">
-                                <p>Date</p>
+                            <TableCell className="w-1/6">
+                                <p>Date :</p>
                             </TableCell>
-                            <TableCell>
-                                <p>Message :</p>
+                            <TableCell className="w-1/5">
+                                <p>Type :</p>
                             </TableCell>
-                            <TableCell />
+                            <TableCell className="w-2/5">
+                                <p>Contenu :</p>
+                            </TableCell>
+                            <TableCell className="w-1/5"/>
                         </TableRow>
                     </TableHeader>
                     <TableBody>
                         <TableRow>
                             <TableCell>
-                                <p>12/12/2021</p>
+                                <p>04/02/2025</p>
                             </TableCell>
                             <TableCell>
-                                <p>Test du message affiché par la notif</p>
+                                <p>Acceptation de votre projet</p>
                             </TableCell>
                             <TableCell>
+                                <p>Félicitations, votre nouveau projet "Développement d'un application mobile avec Kotlin" a été accepté par l'équipe pédagogique !</p>
+                            </TableCell>
+                            <TableCell className="flex justify-center">
                                 <Button className="flex items-center" size="sm" variant="ghost">Supprimer</Button>
                             </TableCell>
                         </TableRow>
