6bed01de92377190fc1a25397dc5e8b53dd6da57
I don't know <lea.granier@outlook.fr>
2024-01-12 14:26:04 +0100

    change icon to text for togglemode

--- src/components/ui/mode-toggle.tsx
+++ src/components/ui/mode-toggle.tsx
@@ -4,19 +4,22 @@ import { useTheme } from "@/config/theme-provider";
 
 import { Button } from "@/components/ui/button";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import {Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
+import {light} from "@fortawesome/fontawesome-svg-core/import.macro";
 
 
 
 export function ModeToggle() {
 
     const { setTheme } = useTheme();
+    const inputStyle = 'w-2/4 mb-2.5'
 
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
                 <Button variant="outline" size="icon">
-                    <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                    <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                    <span className=" rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" > Light </span>
+                    <span className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" > Dark </span>
                     <span className="sr-only">Toggle theme</span>
                 </Button>
             </DropdownMenuTrigger>
