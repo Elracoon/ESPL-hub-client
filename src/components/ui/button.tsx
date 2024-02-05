3fedf28448f49ff769443fdcb93f484cf93b9bee
arthur <arthur.broudoux@gmail.com>
2024-02-05 16:48:31 +0100

    change style buttons lg

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -22,7 +22,7 @@ const buttonVariants = cva(
       size: {
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
-        lg: "h-14 rounded-md px-6 text-lg font-normal",
+        lg: "h-12 rounded-md px-4 text-base font-normal",
         xl: "h-14 rounded-lg px-8 text-lg font-semibold",
         icon: "h-10 w-10",
         profile: "w-1/2",
