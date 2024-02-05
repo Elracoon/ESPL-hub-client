8de9cacf56d6fc1ea6fe2d09c34f48f642937117
AubinManceau <aubinmanceau0@gmail.com>
2024-02-05 13:29:37 +0100

    update

--- src/pages/Notifications.tsx
+++ src/pages/Notifications.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import {Table, TableHeader, TableBody, TableRow, TableCell} from "@/components/ui/table"
 import {DropdownMenu, DropdownMenuContent,DropdownMenuItem, DropdownMenuRadioGroup,
     DropdownMenuRadioItem,DropdownMenuTrigger, DropdownMenuSeparator} from "@/components/ui/dropdown-menu"
@@ -7,9 +7,26 @@ import { MoreHorizontal } from "lucide-react"
 
 
 export default function Notifications() {
-    const [position, setPosition] = React.useState("not-read")
+    const [read, setRead] = useState<{ [id: number]: boolean }>({});
+    let button: JSX.Element;
+
+    const toggleReadStatus = (id: number) => {
+        setRead((prevStatus) => ({
+          ...prevStatus,
+          [id]: !prevStatus[id]
+        }));
+    };
+    
+    const getButton = (id: number) => {
+        return read[id] === false ? (
+          <DropdownMenuItem onClick={() => toggleReadStatus(id)}>Marquer comme lu</DropdownMenuItem>
+        ) : (
+          <DropdownMenuItem onClick={() => toggleReadStatus(id)}>Marquer comme non lu</DropdownMenuItem>
+        );
+    };
 
     type notifs = {
+        id: number,
         date: string,
         type: string,
         contenu: string
@@ -17,21 +34,25 @@ export default function Notifications() {
 
     const notifs = [
         {
+            id: 0,
             date: "04/01/2025",
             type: "Acceptation de votre projet",
             contenu: "Félicitations, votre nouveau projet \"Développement d'une application mobile avec Kotlin\" a été accepté par l'équipe pédagogique !"
         },
         {
+            id: 1,
             date: "04/02/2025",
             type: "Nouveau feedback sur votre projet",
             contenu: "Vous avez un nouveau feddback sur votre projet \"Développement d'une application mobile avec Kotlin\""
         },
         {
+            id: 2,
             date: "04/03/2025",
             type: "Nouveau participant à votre projet",
             contenu: "Léa Granier souhaite participer à votre projet \"Développement d'une application mobile avec Kotlin\""
         },
         {
+            id: 3,
             date: "04/04/2025",
             type: "Acceptation de votre candidature",
             contenu: "Félicitations, vous avez été accepté pour participer au projet \"Développement d'une application mobile avec Kotlin\""
@@ -60,25 +81,22 @@ export default function Notifications() {
                         </TableRow>
                     </TableHeader>
                     <TableBody>
-                        {notifs.map(({date, type, contenu}: notifs) => (
+                        {notifs.map(({date, type, contenu, id}: notifs) => (
                             <TableRow>
                                 <TableCell>
-                                    <p className="text-xl" key={date}>{date}</p>
+                                    <p className="text-xl" key={id}>{date}</p>
                                 </TableCell>
                                 <TableCell>
-                                    <p key={type} className="text-primary font-semibold text-xl">{type}</p>
+                                    <p key={id} className="text-primary font-semibold text-xl">{type}</p>
                                 </TableCell>
                                 <TableCell>
-                                    <p className="text-xl" key={contenu}>{contenu}</p>
+                                    <p className="text-xl" key={id}>{contenu}</p>
                                 </TableCell>
                                 <TableCell className="flex justify-center items-center">
                                     <DropdownMenu>
                                         <DropdownMenuTrigger><Button size="sm" variant="ghost"><MoreHorizontal /></Button></DropdownMenuTrigger>
                                         <DropdownMenuContent>
-                                            <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
-                                                <DropdownMenuRadioItem value="not-read">Marquer comme non lu</DropdownMenuRadioItem>
-                                                <DropdownMenuRadioItem value="read">Marquer comme lu</DropdownMenuRadioItem>
-                                            </DropdownMenuRadioGroup>
+                                            {getButton(id)}
                                             <DropdownMenuSeparator />
                                             <DropdownMenuItem className="text-primary font-bold">Supprimer</DropdownMenuItem>
                                         </DropdownMenuContent>
