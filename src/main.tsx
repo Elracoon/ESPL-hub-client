db938e22d36cfd8933088abd68e3ae1e665408b2
arthur <arthur.broudoux@gmail.com>
2024-02-05 12:00:43 +0100

    configure router

--- src/main.tsx
+++ src/main.tsx
@@ -1,33 +1,18 @@
-import { useLocation, RouterProvider, Switch } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import { Toaster } from "sonner";
 
 import { ThemeProvider } from "@/config/theme-provider";
 import router from "@/router/router";
 
-import Nav from "@/components/Header/Nav";
-
 import "@/style/index.css";
 import "@/style/layouts.css";
-
-export function AppRoutes() {
-  const location = useLocation();
-  const showNav = ["/", "/notifications", "/profile"];
-  const shouldShowNav = showNav.includes(location.pathname);
-
-  return (
-    <Switch>
-      {shouldShowNav && <Nav />}
-      <RouterProvider router={router} />
-      <Toaster richColors />
-    </Switch>
-  );
-}
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 ).render(
   <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-    <AppRoutes />
+    <RouterProvider router={router} />
+    <Toaster richColors />
   </ThemeProvider>
 );
