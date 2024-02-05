51c1fada84daecc0767d49c30f2efa0f7397cefd
arthur <arthur.broudoux@gmail.com>
2024-02-05 09:39:56 +0100

    Project Page: v.1.0.0

--- src/components/Cards/CardProject.tsx
+++ src/components/Cards/CardProject.tsx
@@ -4,42 +4,47 @@ import { Link } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 
-
 type CardProjectProps = {
-    title: string;
-    date: string;
-    owner: string;
-    description: string;
-    skills: string[];
-    id?: string;
-}
+  title: string;
+  date: string;
+  owner: string;
+  description: string;
+  skills: string[];
+  id?: string;
+};
 
-const CardProject : FC<CardProjectProps> = ( props ) => {
+const CardProject: FC<CardProjectProps> = (props) => {
+  const maxLenghtDescription = 230;
+  const lenghtDescription = (props: any) => {
+    if (props.description.length > maxLenghtDescription) {
+      return props.description.slice(0, maxLenghtDescription) + " ...";
+    }
+    return props.description;
+  };
 
-    const maxLenghtDescription = 230;
-    const lenghtDescription = ( props:any ) => {
-        if ( props.description.length > maxLenghtDescription ) {
-            return props.description.slice(0, maxLenghtDescription) + ' ...';
-        };
-        return props.description;
-    };
-
-    return (
-        <div className="w-[31%] h-auto bg-background border-2 rounded-lg p-6 text-xl text-white font-medium">
-            <p className="flex-row-center-start gap-2">
-                {props.skills.map((tag, index) => (
-                            <Badge key={index} variant="outline" className="text-sm">{ tag }</Badge>
-                        ))}
-            </p>
-            <h3 className="text-3xl pb-4 pt-2 font-bold">{ props.title }</h3>
-            <p className="text-muted-foreground font-light text-base">Projet crée par <span className="text-white hover:underline hover:cursor-pointer">{ props.owner }</span> le <span className="text-white">{ props.date }</span></p>
-            <p className="pt-6 pb-10 text-lg">{ lenghtDescription(props) }</p>
-            <Link to={`/project/${props.id}`}>
-                <Button size={"lg"}>En savoir plus</Button>
-            </Link>
-        </div>
-    )
-}
-
+  return (
+    <div className="w-[31%] h-auto bg-background border-2 rounded-lg p-6 text-xl text-white font-medium">
+      <p className="flex-row-center-start gap-2">
+        {props.skills.map((tag, index) => (
+          <Badge key={index} variant="outline" className="text-sm">
+            {tag}
+          </Badge>
+        ))}
+      </p>
+      <h3 className="text-3xl pb-4 pt-2 font-bold">{props.title}</h3>
+      <p className="text-muted-foreground font-light text-base">
+        Projet crée par{" "}
+        <span className="text-white hover:underline hover:cursor-pointer">
+          {props.owner}
+        </span>{" "}
+        le <span className="text-white">{props.date}</span>
+      </p>
+      <p className="pt-6 pb-10 text-lg">{lenghtDescription(props)}</p>
+      <Link to={`/project/${props.id}`}>
+        <Button size={"lg"}>En savoir plus</Button>
+      </Link>
+    </div>
+  );
+};
 
 export default CardProject;
--- src/components/Tabs/TabsMain.tsx
+++ src/components/Tabs/TabsMain.tsx
@@ -2,17 +2,31 @@ import HeaderTab from "@/components/Tabs/HeaderTab";
 import CardProject from "@/components/Cards/CardProject";
 import { TabsContent } from "@/components/ui/tabs";
 
-
 export default function TabsMain() {
-    return (
-        <TabsContent value="projets" className="tabs-content-dashboard">
-            <div className="w-full h-auto pt-2 pb-10 text-2xl flex-col-start-start gap-4 font-semibold">
-                <HeaderTab title="Projets" description="Consultez les derniers projets publiés et commencez dès maintenant à répondre à des offres" />
-            </div>
-            <div className="flex-row-center gap-4">
-                <CardProject title="Un très long titre pour faire un très long test" date="20/05/2004" description="Voici une énorme description ou je parle de ce super projet que tout le monde devrait rejoindre pour faire un super projet avec de supers technos qui va générer beaucpup d'argent vous devriez tous rejoindre ce projet genre vraiment" owner="Arthur" skills={["dev", "seo"]} />
-                <CardProject title="Un très long titre pour faire un très long test" date="20/05/2004" description="Voici une énorme description ou je parle de ce super projet que tout le monde devrait rejoindre pour faire un super projet avec de supers technos qui va générer beaucpup d'argent vous devriez tous rejoindre ce projet genre vraiment" owner="Arthur" skills={["dev", "seo"]} />
-            </div>
-        </TabsContent>
-    )
+  return (
+    <TabsContent value="projets" className="tabs-content-dashboard">
+      <div className="w-full h-auto pt-2 pb-10 text-2xl flex-col-start-start gap-4 font-semibold">
+        <HeaderTab
+          title="Projets"
+          description="Consultez les derniers projets publiés et commencez dès maintenant à répondre à des offres"
+        />
+      </div>
+      <div className="flex-row-center gap-4">
+        <CardProject
+          title="Un très long titre pour faire un très long test"
+          date="20/05/2004"
+          description="Voici une énorme description ou je parle de ce super projet que tout le monde devrait rejoindre pour faire un super projet avec de supers technos qui va générer beaucpup d'argent vous devriez tous rejoindre ce projet genre vraiment"
+          owner="Arthur"
+          skills={["dev", "seo"]}
+        />
+        <CardProject
+          title="Un très long titre pour faire un très long test"
+          date="20/05/2004"
+          description="Voici une énorme description ou je parle de ce super projet que tout le monde devrait rejoindre pour faire un super projet avec de supers technos qui va générer beaucpup d'argent vous devriez tous rejoindre ce projet genre vraiment"
+          owner="Arthur"
+          skills={["dev", "seo"]}
+        />
+      </div>
+    </TabsContent>
+  );
 }
