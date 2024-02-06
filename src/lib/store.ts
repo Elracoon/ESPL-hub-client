e11956a90da08febb520b217913c6fa0ca5c2aa6
arthur <arthur.broudoux@gmail.com>
2024-02-06 09:45:47 +0100

    Page Profile, Nav, FirstLetterUsername

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -10,6 +10,7 @@ interface Store {
   isLoading: boolean;
   isCreating: boolean;
   username: string;
+  firstLetterUsername: string;
   haveNotifs: boolean;
   signIn: () => void;
   signOut: () => void;
@@ -18,6 +19,7 @@ interface Store {
   setIsLoading: (value: boolean) => void;
   setIsCreating: (value: boolean) => void;
   setUsername: (value: string) => void;
+  setFirstLetterUsername: (value: string) => void;
   setHaveNotifs: (value: boolean) => void;
 }
 
@@ -29,14 +31,15 @@ const useStore = create<Store>((set) => ({
   isCreating: false,
   username: username ? JSON.parse(username) : "",
   haveNotifs: false,
+  firstLetterUsername: username ? username[1].toUpperCase() : "",
   signIn: () => {
     set({ token: true });
-    localStorage.setItem("token", JSON.stringify(true));
+    sessionStorage.setItem("token", JSON.stringify(true));
   },
   signOut: () => {
     set({ token: false });
-    localStorage.removeItem("token");
-    localStorage.removeItem("firstName");
+    sessionStorage.removeItem("token");
+    sessionStorage.removeItem("firstName");
   },
   setIsDeleting: (value: boolean) => set({ isDeleting: value }),
   setIsUpdating: (value: boolean) => set({ isUpdating: value }),
@@ -44,9 +47,11 @@ const useStore = create<Store>((set) => ({
   setIsCreating: (value: boolean) => set({ isCreating: value }),
   setUsername: (value: string) => {
     set({ username: value });
-    localStorage.setItem("username", JSON.stringify(value));
+    sessionStorage.setItem("username", JSON.stringify(value));
   },
   setHaveNotifs: (value: boolean) => set({ haveNotifs: value }),
+  setFirstLetterUsername: (value: string) =>
+    set({ firstLetterUsername: value }),
 }));
 
 export default useStore;
