b834d602bf646cc111c06143fc4b52b9b3a12825
ElRacoon <lea.granier@outlook.fr>
2024-02-06 12:22:30 +0100

    fix front in white mode

--- src/components/Cards/CardProject.tsx
+++ src/components/Cards/CardProject.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
+import {useTheme} from "@/config/theme-provider";
 
 type CardProjectProps = {
   title: string;
@@ -14,39 +15,46 @@ type CardProjectProps = {
 };
 
 const CardProject: FC<CardProjectProps> = (props) => {
+  const { theme } = useTheme();
   const maxLenghtDescription = 230;
-  const lenghtDescription = (props: any) => {
-    if (props.description.length > maxLenghtDescription) {
-      return props.description.slice(0, maxLenghtDescription) + " ...";
+
+  const lengthDescription = (description: string) => {
+    if (description.length > maxLenghtDescription) {
+      return description.slice(0, maxLenghtDescription) + " ...";
     }
-    return props.description;
+    return description;
   };
 
   return (
-    <Link
-      className="w-[31%] h-auto bg-background border-2 rounded-lg p-6 text-xl text-white font-medium"
-      to={`/project/${props.id}`}
-    >
-      <p className="flex-row-center-start gap-2">
-        {props.skills.map((tag, index) => (
-          <Badge key={index} variant="outline" className="text-sm">
-            {tag}
-          </Badge>
-        ))}
-      </p>
-      <h3 className="text-3xl pb-4 pt-2 font-bold">{props.title}</h3>
-      <p className="text-muted-foreground font-light text-base">
-        Projet crée par{" "}
-        <span className="text-white hover:underline hover:cursor-pointer">
+      <Link
+          className={`w-[31%] h-auto bg-background border-2 rounded-lg p-6 text-xl font-medium ${
+              theme === "light" ? "text-black" : "text-white"
+          }`}
+          to={`/project/${props.id}`}
+      >
+        <div className="flex-row-center-start gap-2">
+          {props.skills.map((tag, index) => (
+              <Badge key={index} variant="outline" className="text-sm">
+                {tag}
+              </Badge>
+          ))}
+        </div>
+        <h3 className="text-3xl pb-4 pt-2 font-bold">{props.title}</h3>
+        <p className="text-muted-foreground font-light text-base">
+          Projet créé par{" "}
+          <span className={theme === "light" ? "text-black" : "text-white"}>
           {props.owner}
         </span>{" "}
-        le <span className="text-white">{props.date}</span>
-      </p>
-      <p className="pt-6 pb-10 text-lg">{lenghtDescription(props)}</p>
-      <Link to={`/project/${props.id}`}>
-        <Button size={"lg"}>En savoir plus</Button>
+          le{" "}
+          <span className={theme === "light" ? "text-black" : "text-white"}>
+          {props.date}
+        </span>
+        </p>
+        <p className="pt-6 pb-10 text-lg">{lengthDescription(props.description)}</p>
+        <Link to={`/project/${props.id}`}>
+          <Button size="lg">En savoir plus</Button>
+        </Link>
       </Link>
-    </Link>
   );
 };
 
