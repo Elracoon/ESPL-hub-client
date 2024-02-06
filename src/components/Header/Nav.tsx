9994e6daa24e0890e5ed5d37a7263217ec57e643
arthur <arthur.broudoux@gmail.com>
2024-02-06 09:00:15 +0100

    Logo, setUsername

--- src/components/Header/Nav.tsx
+++ src/components/Header/Nav.tsx
@@ -21,80 +21,105 @@ import { Textarea } from "@/components/ui/textarea";
 
 export default function Nav() {
   const [firstLetterUsername, setFirstLetterUsername] = useState("");
-  const { firstName } = useStore();
-  const [firstLetterFirstName, setFirstLetterFirstName] = useState('');
-  const { haveNotifs } = useStore()
+  const { username } = useStore();
+  const [firstLetterFirstName, setFirstLetterFirstName] = useState("");
+  const { haveNotifs } = useStore();
 
   useEffect(() => {
-      if (firstName && firstName.length > 0) {
-          setFirstLetterFirstName(firstName.charAt(0).toUpperCase());
-      };
-  }, [firstName]);
+    if (username && username.length > 0) {
+      setFirstLetterUsername(username.charAt(0).toUpperCase());
+    }
+  }, [username]);
 
   const haveUnreadNotifs = () => {
-      return (haveNotifs === true) ? (
-          <div>
-              <Bell />
-              <div className="absolute right-0 bottom-0 h-4 w-4 bg-primary rounded-full"></div>
-          </div>
-      ) : (
-          <Bell />
-      );
-  }
+    return haveNotifs === true ? (
+      <div>
+        <Bell />
+        <div className="absolute right-0 bottom-0 h-4 w-4 bg-primary rounded-full"></div>
+      </div>
+    ) : (
+      <Bell />
+    );
+  };
 
   return (
     <nav className="w-full h-32 py-4 px-6 flex-row-center-between">
-        <div className="flex-row-center-center">
-            <h1 className="text-5xl font-semibold">
-                <Link to={"/"}>ESPL Hub</Link>
-            </h1>
-        </div>
-        <div className="flex-row-center-between gap-4">
-            <Dialog>
-                <DialogTrigger asChild>
-                    <Button>Create +</Button>
-                </DialogTrigger>
-                <DialogContent className="sm:max-w-[450px]">
-                    <DialogHeader>
-                        <DialogTitle>Créez un projet</DialogTitle>
-                        <DialogDescription>
-                            Décrivez votre projet
-                        </DialogDescription>
-                    </DialogHeader>
-                    <div className="grid gap-5 py-4">
-                        <div className="grid grid-cols-4 items-center gap-5">
-                            <Label htmlFor="nameProject" className="text-right">Titre</Label>
-                            <Input id="nameProject" placeholder="Titre" className="col-span-3" />
-                        </div>
-                        <div className="grid grid-cols-4 items-center gap-5">
-                            <Label htmlFor="goalsProject" className="text-right">Objectifs</Label>
-                            <Input id="goalsProject" placeholder="Objectifs" className="col-span-3" />
-                        </div>
-                        <div className="grid grid-cols-4 items-center gap-5">
-                            <Label htmlFor="skillsProject" className="text-right">Compétences</Label>
-                            <Input id="skillsProject" placeholder="Compétences recherchées" className="col-span-3" />
-                        </div>
-                        <div className="grid grid-cols-4 items-start gap-5">
-                            <Label htmlFor="skillsProject" className="text-right">Description</Label>
-                            <Textarea name="description" className="col-span-3" placeholder="Description" />
-                        </div>
-                    </div>
-                    <DialogFooter>
-                        <Button type="submit">Créer</Button>
-                    </DialogFooter>
-                </DialogContent>
-            </Dialog>
-            <Link to={"/notifications"}>
-                <div className="flex-col-center-center rounded-full h-10 w-10 relative">
-                    {haveUnreadNotifs()}
-                </div>
-            </Link>
-            <Link to={"/profile"}>
-                <Avatar>
-                    <AvatarFallback>{ firstLetterFirstName }</AvatarFallback>
-                </Avatar>
-            </Link>
-        </div>
+      <div className="flex-row-center-center">
+        <h1 className="text-2xl font-semibold">
+          <Link to={"/"} className="flex-row-center-start gap-3">
+            ESPL Hub
+            <Blocks className="text-primary text-3xl" />
+          </Link>
+        </h1>
+      </div>
+      <div className="flex-row-center-between gap-4">
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button>Create +</Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[450px]">
+            <DialogHeader>
+              <DialogTitle>Créez un projet</DialogTitle>
+              <DialogDescription>Décrivez votre projet</DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-5 py-4">
+              <div className="grid grid-cols-4 items-center gap-5">
+                <Label htmlFor="nameProject" className="text-right">
+                  Titre
+                </Label>
+                <Input
+                  id="nameProject"
+                  placeholder="Titre"
+                  className="col-span-3"
+                />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-5">
+                <Label htmlFor="goalsProject" className="text-right">
+                  Objectifs
+                </Label>
+                <Input
+                  id="goalsProject"
+                  placeholder="Objectifs"
+                  className="col-span-3"
+                />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-5">
+                <Label htmlFor="skillsProject" className="text-right">
+                  Compétences
+                </Label>
+                <Input
+                  id="skillsProject"
+                  placeholder="Compétences recherchées"
+                  className="col-span-3"
+                />
+              </div>
+              <div className="grid grid-cols-4 items-start gap-5">
+                <Label htmlFor="skillsProject" className="text-right">
+                  Description
+                </Label>
+                <Textarea
+                  name="description"
+                  className="col-span-3"
+                  placeholder="Description"
+                />
+              </div>
+            </div>
+            <DialogFooter>
+              <Button type="submit">Créer</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+        <Link to={"/notifications"}>
+          <div className="flex-col-center-center rounded-full h-10 w-10 relative">
+            {haveUnreadNotifs()}
+          </div>
+        </Link>
+        <Link to={"/profile"}>
+          <Avatar>
+            <AvatarFallback>{firstLetterUsername}</AvatarFallback>
+          </Avatar>
+        </Link>
+      </div>
     </nav>
   );
 }
--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,52 +1,52 @@
 import { create } from "zustand";
 
 const savedToken = sessionStorage.getItem("token");
-const firstName = sessionStorage.getItem("firstName");
+const username = sessionStorage.getItem("username");
 
 interface Store {
-    token: boolean;
-    isDeleting: boolean;
-    isUpdating: boolean;
-    isLoading: boolean;
-    isCreating: boolean;
-    firstName: string;
-    haveNotifs: boolean;
-    signIn: () => void;
-    signOut: () => void;
-    setIsDeleting: (value: boolean) => void;
-    setIsUpdating: (value: boolean) => void;
-    setIsLoading: (value: boolean) => void;
-    setIsCreating: (value: boolean) => void;
-    setFirstName: (value: string) => void;
-    setHaveNotifs: (value: boolean) => void;
-};
+  token: boolean;
+  isDeleting: boolean;
+  isUpdating: boolean;
+  isLoading: boolean;
+  isCreating: boolean;
+  username: string;
+  haveNotifs: boolean;
+  signIn: () => void;
+  signOut: () => void;
+  setIsDeleting: (value: boolean) => void;
+  setIsUpdating: (value: boolean) => void;
+  setIsLoading: (value: boolean) => void;
+  setIsCreating: (value: boolean) => void;
+  setUsername: (value: string) => void;
+  setHaveNotifs: (value: boolean) => void;
+}
 
 const useStore = create<Store>((set) => ({
-    token: savedToken ? JSON.parse(savedToken) : false,
-    isDeleting: false,
-    isUpdating: false,
-    isLoading: false,
-    isCreating: false,
-    firstName: firstName ? JSON.parse(firstName) : '',
-    haveNotifs: false,
-    signIn: () => {
-        set({ token: true });
-        localStorage.setItem('token', JSON.stringify(true));
-    },
-    signOut: () => {
-        set({ token: false });
-        localStorage.removeItem('token');
-        localStorage.removeItem('firstName');
-    },
-    setIsDeleting: (value: boolean) => set({ isDeleting: value }),
-    setIsUpdating: (value: boolean) => set({ isUpdating: value }),
-    setIsLoading: (value: boolean) => set({ isLoading: value }),
-    setIsCreating: (value: boolean) => set({ isCreating: value }),
-    setFirstName: (value: string) => {
-        set({ firstName: value});
-        localStorage.setItem('firstName', JSON.stringify(value));
-    },
-    setHaveNotifs: (value: boolean) => set({ haveNotifs: value }),
+  token: savedToken ? JSON.parse(savedToken) : false,
+  isDeleting: false,
+  isUpdating: false,
+  isLoading: false,
+  isCreating: false,
+  username: username ? JSON.parse(username) : "",
+  haveNotifs: false,
+  signIn: () => {
+    set({ token: true });
+    localStorage.setItem("token", JSON.stringify(true));
+  },
+  signOut: () => {
+    set({ token: false });
+    localStorage.removeItem("token");
+    localStorage.removeItem("firstName");
+  },
+  setIsDeleting: (value: boolean) => set({ isDeleting: value }),
+  setIsUpdating: (value: boolean) => set({ isUpdating: value }),
+  setIsLoading: (value: boolean) => set({ isLoading: value }),
+  setIsCreating: (value: boolean) => set({ isCreating: value }),
+  setUsername: (value: string) => {
+    set({ username: value });
+    localStorage.setItem("username", JSON.stringify(value));
+  },
+  setHaveNotifs: (value: boolean) => set({ haveNotifs: value }),
 }));
 
 export default useStore;
